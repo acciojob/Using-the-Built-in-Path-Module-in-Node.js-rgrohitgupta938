5b191ddd3b6e6cc4d8629bad55a8a7043a8010aa
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-21 18:22:18 +0530

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const path = require('path');
+const path = require("path");
+
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+  console.error("Please provide exactly two file path segments.");
+  process.exit(1);
+}
+const [seg1, seg2] = args;
+
+const joinedPath = path.join(seg1, seg2);
+
+console.log(joinedPath);
 
 // TODO: Use path.join to join two file path segments
 
